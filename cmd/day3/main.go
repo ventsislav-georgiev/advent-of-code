2a0bbf63eb1e011562a418784d864364d573c9d0
ventsislav-georgiev <ventsislav.georgiev@live.com>
2022-12-03 11:22:06 +0200

    feat: day 3

---
+++ cmd/day3/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/ventsislav-georgiev/advent-of-code-22/pkg/aoc"
+)
+
+func main() {
+	task := aoc.GetTask(task1, task2)
+	in := aoc.GetInput(3)
+	defer in.Close()
+	task(in)
+}
+
+func task1(in io.ReadCloser) {
+	scanner := bufio.NewScanner(in)
+
+	result := 0
+	for scanner.Scan() {
+		r := scanner.Text()
+		halflen := len(r) / 2
+
+		bitmap := toBitmap(r[:halflen]) & toBitmap(r[halflen:])
+		for p := 0; p <= 52; p++ {
+			if bitmap&(1<<p) != 0 {
+				result += p + 1
+			}
+		}
+	}
+
+	fmt.Println(result)
+}
+
+func task2(in io.ReadCloser) {
+	scanner := bufio.NewScanner(in)
+
+	result := 0
+	for scanner.Scan() {
+		r1 := scanner.Text()
+		scanner.Scan()
+		r2 := scanner.Text()
+		scanner.Scan()
+		r3 := scanner.Text()
+
+		bitmap := toBitmap(r1) & toBitmap(r2) & toBitmap(r3)
+		for p := 0; p <= 52; p++ {
+			if bitmap&(1<<p) != 0 {
+				result += p + 1
+				break
+			}
+		}
+	}
+
+	fmt.Println(result)
+}
+
+func toBitmap(s string) uint64 {
+	var bitmap uint64
+	for _, ch := range s {
+		if ch >= 'a' {
+			bitmap |= 1 << (ch - 'a')
+		} else {
+			bitmap |= 1 << (ch - 'A' + 26)
+		}
+	}
+	return bitmap
+}
