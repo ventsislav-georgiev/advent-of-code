c5d2a26582878ce1404724f40cf3404767d8814c
Ventsislav Georgiev <ventsislav.georgiev@live.com>
2023-12-10 22:10:11 +0200

    feat(2019): day 20 part 1

--- golang/cmd/2019/20/main.go
+++ golang/cmd/2019/20/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"image"
 	"io"
-	"strings"
 
 	"github.com/ventsislav-georgiev/advent-of-code/golang/pkg/aoc"
 )
@@ -14,26 +14,6 @@ func main() {
 }
 
 func task1(in io.Reader) {
-	in = strings.NewReader(`
-         A           
-         A           
-  #######.#########  
-  #######.........#  
-  #######.#######.#  
-  #######.#######.#  
-  #######.#######.#  
-  #####  B    ###.#  
-BC...##  C    ###.#  
-  ##.##       ###.#  
-  ##...DE  F  ###.#  
-  #####    G  ###.#  
-  #########.#####.#  
-DE..#######...###.#  
-  #.#########.###.#  
-FG..#########.....#  
-  ###########.#####  
-             Z       
-             Z       `)
 	grid, size := parse(in)
 
 	for y := 0; y < size.Y; y++ {
@@ -50,33 +30,83 @@ FG..#########.....#
 			}
 
 			if _, ok := grid.firstImmediateNeighbourPath(pos1); ok {
-				grid.updateLetters(pos1, pos2, pos1, pos1)
-			} else {
-				grid.updateLetters(pos1, pos2, pos1, pos2)
+				grid.updateLetters(pos1, pos2)
 			}
 		}
 	}
 
+	coords := CordsMap{}
 	for y := 0; y < size.Y; y++ {
 		for x := 0; x < size.X; x++ {
-			print(grid[image.Pt(x, y)])
+			p := image.Pt(x, y)
+			id := grid[p]
+
+			if len(id) < 2 {
+				continue
+			}
+
+			if _, ok := coords[id]; ok {
+				id += "*"
+				grid[p] = id
+			}
+
+			coords[id] = p
 		}
-		println()
 	}
+
+	start := coords["AA"]
+	end := coords["ZZ"]
+	queue := []image.Point{start}
+	distances := map[image.Point]int{start: -1}
+
+	for len(queue) > 0 {
+		cur := queue[0]
+		queue = queue[1:]
+
+		if cur == end {
+			continue
+		}
+
+		for _, dir := range directions {
+			next := cur.Add(dir)
+			if d, ok := distances[next]; ok && d <= distances[cur]+1 {
+				continue
+			}
+
+			id := grid[next]
+			if id == "#" || id == " " {
+				continue
+			}
+
+			var portalID string
+			if len(id) == 3 {
+				portalID = id[:2]
+			} else if len(id) == 2 {
+				portalID = id + "*"
+			}
+
+			coord, foundPortal := coords[portalID]
+			distCount := 1
+			if foundPortal {
+				next = coord
+				distCount = 0
+			}
+
+			queue = append(queue, next)
+			distances[next] = distances[cur] + distCount
+		}
+	}
+
+	fmt.Println(distances[end] - 1)
 }
 
 func task2(in io.Reader) {
 }
 
-type Node struct {
-	val   string
-	pos   image.Point
-	edges []*Node
-}
-
 var directions = []image.Point{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
 
 type Grid map[image.Point]string
+type CordsMap map[string]image.Point
 
 func (g Grid) firstImmediateNeighbourLetter(src image.Point) (image.Point, bool) {
 	for _, dir := range directions {
@@ -96,14 +126,14 @@ func (g Grid) firstImmediateNeighbourPath(src image.Point) (image.Point, bool) {
 	return image.Point{}, false
 }
 
-func (g Grid) updateLetters(pos1, pos2, srcPos, destPos image.Point) {
-	if pos2.X > pos1.X || pos2.Y > pos1.Y {
-		g[destPos] = g[pos1] + g[pos2]
-		g[pos2] = " "
+func (g Grid) updateLetters(pos1, pos2 image.Point) {
+	if pos1.X > pos2.X || pos1.Y > pos2.Y {
+		g[pos1] = g[pos2] + g[pos1]
 	} else {
-		g[destPos] = g[pos2] + g[pos1]
-		g[pos1] = " "
+		g[pos1] = g[pos1] + g[pos2]
 	}
+
+	g[pos2] = " "
 }
 
 func parse(in io.Reader) (Grid, image.Point) {
